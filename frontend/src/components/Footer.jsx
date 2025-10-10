f8499bf869405720b6c90d11ec2a246dc891ca51
Reutechhub <callmeyoungben@gmail.com>
2025-10-10 05:13:28 +0200

    FOOTER cl1

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-r from-primary to-secondary text-white py-4 mt-5" style={{background: 'linear-gradient(90deg, #0d6efd 0%, #6610f2 100%)'}}>
+    <footer className="bg-gradient-to-r from-primary to-secondary text-white py-4 mt-5" style={{background: 'linear-gradient(90deg, #000000ff 0%, #240d49ff 100%)'}}>
       <div className="container d-flex justify-content-center align-items-center text-center">
         <div>
           <h6 className="mb-0 fw-bold" style={{ fontSize: '0.9rem' }}> MATERIALS AT HAND</h6>
-          <small style={{ fontSize: '0.7rem' }}><>Designed by REUTECH HUB INNOVATIONS</></small>
+          <small style={{ fontSize: '0.5rem' }}><>Designed by REUTECH HUB INNOVATIONS</></small>
         </div>
       </div>
     </footer>
